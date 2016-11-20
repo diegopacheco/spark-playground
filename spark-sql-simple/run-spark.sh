428a3c96c8b8d1f65f43f6b5e82ffa809e97120f
diegopacheco <diego.pacheco.it@gmail.com>
2016-11-20 03:15:48 -0200

    added spark sql code

---
+++ spark-sql-simple/run-spark.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+SPARK_HOME=/home/diego/bin/spark-2.0.2-bin-hadoop2.7/
+MAIN_CLASS="com.github.diegopacheco.spark.simple.SimpleSparkSQLApp"
+JAR_NAME="spark-sql-simple_2.11-1.0.jar"
+
+$SPARK_HOME/bin/spark-submit \
+  --class $MAIN_CLASS \
+  --master local[4] \
+  target/scala-2.11/$JAR_NAME
+
---
+++ spark-sql-simple/src/main/scala/com/github/diegopacheco/spark/simple/SimpleSparkSQLApp.scala
@@ -0,0 +1,32 @@
+package com.github.diegopacheco.spark.simple
+
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
+
+object SimpleSparkSQLApp extends App {
+
+  val spark = SparkSession
+    .builder()
+    .appName("SimpleSparkSQLApp")
+    .config("spark.some.config.option", "some-value")
+    .getOrCreate()
+  
+  import spark.implicits._  
+  
+  case class Person(name: String, age: Long)
+  val caseClassDS = Seq(Person("Andy", 32)).toDS()
+  val r1 = caseClassDS.show()
+  println(r1)
+  
+  val primitiveDS = Seq(1, 2, 3).toDS()
+  val r2 = primitiveDS.map(_ + 1).collect()
+  println(r2)
+  
+  Seq(Person("Andy", 32)).toDF().createOrReplaceTempView("people")
+  
+  val teenagersDF = spark.sql("SELECT name, age FROM people WHERE age BETWEEN 13 AND 33")
+  teenagersDF.map(teenager => "Name: " + teenager(0)).show()
+  
+  spark.stop()
+  
+}
