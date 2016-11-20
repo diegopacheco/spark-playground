94e8a46a9397c8cc24a1771cfe0d3018bc809d7f
diegopacheco <diego.pacheco.it@gmail.com>
2016-11-20 01:57:17 -0200

    added word count with more fun

--- spark-simple/src/main/scala/com/github/diegopacheco/spark/simple/SimpleSparkApp.scala
+++ spark-simple/src/main/scala/com/github/diegopacheco/spark/simple/SimpleSparkApp.scala
@@ -11,10 +11,23 @@ object SimpleSparkApp extends App {
   
   val fileLocation = "src/main/resources/data.txt"
   
-  val lines = sc.textFile(fileLocation)
+  val lines = sc.textFile(fileLocation).cache()
   val lineLengths = lines.map(s => s.length)
   val totalLength = lineLengths.reduce((a, b) => a + b)
   println(totalLength)
+  
+  val numAs = lines.filter(line => line.contains("A")).count()
+  val numBs = lines.filter(line => line.contains("B")).count()
+  println(s"TOTAL = A -> ${numAs} B -> ${numBs}")
+ 
+  val wordCounts = lines.flatMap(line => line.split(" ")).
+                         filter(!_.headOption.exists(_.isDigit)).
+                         map(word => (word, 1)).
+                         reduceByKey((a, b) => a + b)
+  
+  wordCounts.collect().sortBy(_._2)(Ordering[Int].reverse).foreach { 
+     case (w,c) => println(s"Word: [${w}] Count: ${c}")
+  }
   
   sc.stop()
   
