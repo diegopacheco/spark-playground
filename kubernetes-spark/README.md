2410bac3648ae72a5b28f5d24ca34402d3b4c8a1
Diego Pacheco <diego.pacheco.it@gmail.com>
2019-12-30 16:32:16 -0300

    deploy spark at k8s

--- kubernetes-spark/README.md
+++ kubernetes-spark/README.md
@@ -1,4 +1,6 @@
 ## Start minikube spark profile
 ```bash
 m start -p spark
+./bake-build-docker-spark-image.sh
+./deploy-spark-k8s.sh
 ```
--- kubernetes-spark/bake-build-docker-spark-image.sh
+++ kubernetes-spark/bake-build-docker-spark-image.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-eval $(minikube docker-env)
+eval $(minikube docker-env -p spark)
 docker build -t diegopacheco/spark:2.4.4 .
