a1319180628de4c0f8bbb3c249e5de266f012f5e
diegopacheco <diego.pacheco.it@gmail.com>
2024-12-29 00:24:45 -0800

    iceberg-spark

---
+++ iceberg-spark/spark-sql.sql
@@ -0,0 +1,13 @@
+CREATE TABLE demo.nyc.taxis(
+  vendor_id bigint,
+  trip_id bigint,
+  trip_distance float,
+  fare_amount double,
+  store_and_fwd_flag string
+)
+PARTITIONED BY (vendor_id);
+
+INSERT INTO demo.nyc.taxis
+VALUES (1, 1000371, 1.8, 15.32, 'N'), (2, 1000372, 2.5, 22.15, 'N'), (2, 1000373, 0.9, 9.01, 'N'), (1, 1000374, 8.4, 42.13, 'Y');
+
+SELECT * FROM demo.nyc.taxis;
